1a443fb0f2c3b9364def1e7dc3a69d4869d3cbca
Solmaragón <msolmaragonfwd@gmail.com>
2026-03-20 11:13:20 -0600

    Implementación de  3 components nuevos y otros 2 en el pages, cuales son la Historia, biodivercidad de la Angostura y el landing page junto con el navbar.

---
+++ ReacProyecto/src/pages/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Landing from '../components/Landing.jsx'
+import Navbar from '../components/Navbar.jsx'
+
+function LandingPage() {
+  return (
+    <div>
+        <Navbar />
+        <Landing />
+    </div>
+  )
+}
+
+export default LandingPage
